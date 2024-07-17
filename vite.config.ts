102277a6eeaa37f64f087bc80bd40b25aa435899
hotdogc1017 <hotdogc1017@gmail.com>
2024-07-17 19:06:52 +0800

    chore: 引入依赖prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json' // Node 14 & 16
-import manifest from './manifest.json' assert { type: 'json' } // Node >=17
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+import manifest from "./manifest.json" assert { type: "json" };
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    crx({ manifest }),
-  ],
-})
+  plugins: [vue(), crx({ manifest })],
+});
