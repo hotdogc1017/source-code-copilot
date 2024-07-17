b6687263e2f969c1e8c1e52c6a4d205e2eb8cb2f
hotdogc1017 <hotdogc1017@gmail.com>
2024-07-17 19:38:42 +0800

    chore: 引入依赖tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+
+const msg = ref("Hello world");
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <h1>{{ msg }}</h1>
+    <input v-model="msg" />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
